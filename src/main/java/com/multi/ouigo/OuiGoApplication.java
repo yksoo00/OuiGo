66894a5eeff209ab97724928cd24878c7e39d4b2
kyungsoo <yksoo00@naver.com>
2025-11-06 16:51:00 +0900

    chore : 기본 프로젝트 셋팅

---
+++ src/main/java/com/multi/ouigo/OuiGoApplication.java
@@ -0,0 +1,13 @@
+package com.multi.ouigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuiGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuiGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/multi/ouigo/OuiGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.ouigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuiGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
