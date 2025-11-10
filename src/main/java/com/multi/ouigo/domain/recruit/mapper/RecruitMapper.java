07bac1ed34a95cbb32a6c6012fea233f42022c7a
kyungsoo <yksoo00@naver.com>
2025-11-11 01:19:30 +0900

    feat: 승인과 조건 리스트를 구현 후 toResDto에 qualifiedByName로 매핑해서 사용

--- src/main/java/com/multi/ouigo/domain/recruit/mapper/RecruitMapper.java
+++ src/main/java/com/multi/ouigo/domain/recruit/mapper/RecruitMapper.java
@@ -1,10 +1,18 @@
 package com.multi.ouigo.domain.recruit.mapper;
 
+import com.multi.ouigo.domain.approval.dto.res.ApprovalResDto;
+import com.multi.ouigo.domain.approval.entity.Approval;
+import com.multi.ouigo.domain.condition.dto.res.ConditionResDto;
+import com.multi.ouigo.domain.condition.entity.Condition;
 import com.multi.ouigo.domain.recruit.dto.req.CreateRecruitReqDto;
 import com.multi.ouigo.domain.recruit.dto.res.RecruitListResDto;
+import com.multi.ouigo.domain.recruit.dto.res.RecruitResDto;
 import com.multi.ouigo.domain.recruit.entity.Recruit;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface RecruitMapper {
@@ -13,7 +21,6 @@ public interface RecruitMapper {
     @Mapping(target = "content", source = "recruitContent")
     @Mapping(target = "startDate", source = "startDate")
     @Mapping(target = "endDate", source = "endDate")
-    @Mapping(target = "touristSpotId", source = "touristSpotId")
     @Mapping(target = "category", source = "recruitCategory")
     @Mapping(target = "approvals", ignore = true)
     @Mapping(target = "conditions", ignore = true)
@@ -26,4 +33,34 @@ public interface RecruitMapper {
     @Mapping(source = "title", target = "recruitTitle")
     @Mapping(source = "content", target = "recruitContent")
     RecruitListResDto toDto(Recruit recruit);
+
+    @Mapping(target = "recruitTitle", source = "title")
+    @Mapping(target = "recruitContent", source = "content")
+    @Mapping(target = "startDate", source = "startDate")
+    @Mapping(target = "endDate", source = "endDate")
+    @Mapping(target = "approvals", source = "approvals", qualifiedByName = "toApprovalDtoList")
+    @Mapping(target = "conditions", source = "conditions", qualifiedByName = "toConditionDtoList")
+    @Mapping(target = "touristSpotAddress", expression = "java(recruit.getTouristSpot().getAddress())")
+    @Mapping(target = "memberAge", expression = "java(recruit.getMember().getAge())")
+    @Mapping(target = "memberGender", expression = "java(recruit.getMember().getGender())")
+    @Mapping(target = "memberName", expression = "java(recruit.getMember().getNickName())")
+    RecruitResDto toResDto(Recruit recruit);
+
+    @Named("toApprovalDtoList")
+    default List<ApprovalResDto> toApprovalDtoList(List<Approval> approvals) {
+        return approvals.stream()
+            .map(a -> ApprovalResDto.builder()
+                .status(a.getStatus())
+                .build())
+            .collect(Collectors.toList());
+    }
+
+    @Named("toConditionDtoList")
+    default List<ConditionResDto> toConditionDtoList(List<Condition> conditions) {
+        return conditions.stream()
+            .map(c -> ConditionResDto.builder()
+                .code(c.getCode())
+                .build())
+            .collect(Collectors.toList());
+    }
 }
