40a34b5430544cdc1e22a4b48ebc9988a56a285a
DESKTOP-P7D06C6\hbk49 <hbok491@gmail.com>
2025-11-11 16:19:12 +0900

    Refactor: TripCardRepository에 JpaRepository 상속 추가

--- src/main/java/com/multi/ouigo/domain/trip/repository/TripCardRepository.java
+++ src/main/java/com/multi/ouigo/domain/trip/repository/TripCardRepository.java
@@ -2,12 +2,13 @@ package com.multi.ouigo.domain.trip.repository;
 
 
 import com.multi.ouigo.domain.trip.entity.TripCard;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface TripCardRepository {
+public interface TripCardRepository extends JpaRepository<TripCard, Long> {
 
     //특정 회원의 여행 카드 조회
 
